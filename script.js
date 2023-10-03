ca39e32fc870b8ef674e7199012cad89e6aef0a5
ahlyiaLogin <146888435+ahlyiaLogin@users.noreply.github.com>
2023-10-03 16:46:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var pass = "2084272";
+
+function passcheck() {
+	
+	if(document.getElementById('pass1').value == pass){
+		
+	}
+	if(document.getElementById('pass1').value != pass){
+		alert("Incorrect");
+		return false;
+	}
+}
