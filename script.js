b1de7da6185aec5613b1cdb742bb75188f8ed454
ahlyiaLogin <146888435+ahlyiaLogin@users.noreply.github.com>
2023-10-04 10:08:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var pass = "20853128";
 function passcheck() {
 	
 	if(document.getElementById('pass1').value == pass){
-		
+		window.location.href = "https://ahlyia.github.io"
 	}
 	if(document.getElementById('pass1').value != pass){
 		alert("Incorrect");
