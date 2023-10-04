c51ee11a441a5c44285c15b40d7909cd264cb6f9
ahlyiaLogin <146888435+ahlyiaLogin@users.noreply.github.com>
2023-10-04 10:23:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var pass = "20853128";
 
 function passcheck() {
+	var fr=new FileReader();
+	fr.onload=function(){
+	}
 	
-	if(document.getElementById('pass1').value == pass){
-		window.location.href = "https://ahlyia.github.io"
+	if(document.getElementById('pass1').value == fr.readAsText("./NOTthepassword.txt")){
+		window.location.href = "https://ahlyia.github.io";
 	}
 	if(document.getElementById('pass1').value != pass){
 		alert("Incorrect");
