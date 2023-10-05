16d9d0e463667ce8fbef9f7a845b17dcb0c653cd
ahlyiaLogin <146888435+ahlyiaLogin@users.noreply.github.com>
2023-10-05 13:23:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var pass = "20853128";
 
 function passcheck() {
 	if(document.getElementById('pass1').value == pass){
-		window.location.href = "https://Lia-Games.github.io/";
+		window.location.href = "https://Lia-Games.github.io/main/";
 	}
 	if(document.getElementById('pass1').value != pass){
 		alert("Incorrect");
